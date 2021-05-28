ae6bbef7810524ec365652e81fc3577bf0c4197f
Heather <81429446+Hev83@users.noreply.github.com>
2021-05-28 13:42:48 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,135 @@
+dataLayer = [
+    {'pageCategory': 'homepage',
+    'pageSubCategory': 'PAGE - HOMEPAGE',
+    'pageLanguage': 'en',
+    'userCountry': '',
+    'memberType': 'NoLogged',
+    'page404':'false',
+    'articleTag':''
+  }];
+
+
+
+
+
+
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KKLP');
+
+
+(function(i,s,o,b,g,r,p,a,m){i['aggrObjName']=r;i[r]=i[r]||
+function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();i['aggrApp']=p;a=s.createElement(o),
+ m=s.getElementsByTagName(o)[0];a.async=1;a.src=(g);m.parentNode.insertBefore(a,m); i[r].aggregatorServerRoot = b; })
+(window,document,'script','https://rum.lamborghini.com/c1','https://www.lamborghini.com/rum/client.js','statsCli','lamborghiniCom');
+
+
+
+
+(window.BOOMR_mq=window.BOOMR_mq||[])
+    .push(["addVar",{
+        "rua.upush": "true",
+        "rua.cpush":"true",
+        "rua.upre":"true",
+        "rua.cpre":"true",
+        "rua.uprl":"false",
+        "rua.cprl":"false",
+        "rua.cprf":"false",
+        "rua.trans":"SJ-d4765f6f-b807-46b3-a181-3137b2811e7b",
+        "rua.cook":"false",
+        "rua.ims":"false",
+        "rua.ufprl":"false",
+        "rua.cfprl":"true",
+        "rua.isuxp":"",
+        "rua.texp":""
+    }]);
+
+
+
+    !function(a){var e="https://s.go-mpulse.net/boomerang/",
+  t="addEventListener";if("False"=="True")a.BOOMR_config=a.BOOMR_config||{},a.BOOMR_config.PageParams=a.BOOMR_config.PageParams||{},a.BOOMR_config.PageParams.pci=!0,
+  e="https://s2.go-mpulse.net/boomerang/";if(window.BOOMR_API_key="BLNKS-LFKQX-FP5ZV-DX2VK-AJ3AT",
+  function()
+  {function n(e)
+  {a.BOOMR_onload=e&&e.timeStamp||(new Date).getTime()}
+  if(!a.BOOMR||!a.BOOMR.version&&!a.BOOMR.snippetExecuted){a.BOOMR=a.BOOMR||{},a.BOOMR.snippetExecuted=!0;var i,_,o,r=document.createElement("iframe");
+  if(a[t])a[t]("load",n,!1);
+  else if(a.attachEvent)a.attachEvent("onload",n);r.src="javascript:void(0)",r.title="",r.role="presentation",
+  (r.frameElement||r).style.cssText="width:0;height:0;border:0;display:none;",o=document.getElementsByTagName("script")[0],o.parentNode.insertBefore(r,o);
+  try{_=r.contentWindow.document}catch(O){i=document.domain,r.src="javascript:var d=document.open();d.domain='"+i+"';void(0);",
+  _=r.contentWindow.document}_.open()._l=function(){var a=this.createElement("script");
+  if(i)this.domain=i;a.id="boomr-if-as",a.src=e+"BLNKS-LFKQX-FP5ZV-DX2VK-AJ3AT",BOOMR_lstart=(new Date).getTime(),this.body.appendChild(a)},_.write("<bo"+'dy onload="document._l();">'),
+  _.close()}}(),"".length>0)
+  if(a&&"performance"in a&&a.performance&&"function"==typeof a.performance.setResourceTimingBufferSize)a.performance.setResourceTimingBufferSize();!function()
+  {if(BOOMR=a.BOOMR||{},BOOMR.plugins=BOOMR.plugins||{},!BOOMR.plugins.AK)
+  {var e="true"=="true"?1:0,t="",n="llffkudacfpf2yfa6phq-f-fa378654e-clientnsv4-s.akamaihd.net",
+  i={"ak.v":"31","ak.cp":"648985","ak.ai":parseInt("422642",10),"ak.ol":"0",
+  "ak.cr":18,"ak.ipv":4,"ak.proto":"h2","ak.rid":"41e56025","ak.r":41450,"ak.a2":e,
+  "ak.m":"x","ak.n":"essl","ak.bpcip":"90.202.85.0","ak.cport":55088,"ak.gh":"95.101.237.151",
+  "ak.quicv":"","ak.tlsv":"tls1.3","ak.0rtt":"","ak.csrc":"-","ak.acc":"","ak.t":"1621160911",
+  "ak.ak":"hOBiQwZUYzCg5VSAfCLimQ==DxXAK/azr7JKXDBQY+ZSR5oK2mFjVLm8SVUCq521OBZQn+OdNdW93oJYRE8b8xocadqfoiJXoILinNZyJyPjgP14nlwewFVD4OHz/6Q9VJDWtXrCJIN9QjMsh5y7Sh/aZr82xoXsc6crRzDF/8ikUPrhob/ChnLFx6X25iq/T80gn518jCcw2B6vOEH9xzgS7cBGDoALSlArFXa99D/8aPJpjdETLWuBrvVwsEY71Za/ilAg0LVjkCID9Ga3qwDgWrLhe0cCHBz8UYPqd5oMfp5IK2D3EEDsGOU4SwvmZ17X3g7lP55UzFVpEy6x+yIDfqzgzaW0WTiYXgY+yqFEBRTL1xFf+/6LkScfKDHLzciyIFtEf8wLCKTp0ujEcWm4C+Av8Na1xgLrp2A9OF/oHm0jNQbJrUlwnuKi9jajp70=",
+  "ak.pv":"112","ak.dpoabenc":""};
+  if(""!==t)i["ak.ruds"]=t;var _={i:!1,av:function(e){var t="http.initiator";
+  if(e&&(!e[t]||"spa_hard"===e[t]))i["ak.feo"]=void 0!==a.aFeoApplied?1:0,BOOMR.addVar(i)},rv:
+  function()
+  {var a=[
+      "ak.bpcip",
+      "ak.cport",
+      "ak.cr",
+      "ak.csrc",
+      "ak.gh",
+      "ak.ipv",
+      "ak.m",
+      "ak.n",
+      "ak.ol",
+      "ak.proto",
+      "ak.quicv",
+      "ak.tlsv",
+      "ak.0rtt",
+      "ak.r",
+      "ak.acc",
+      "ak.t"];
+      BOOMR.removeVar(a)}};
+      BOOMR.plugins.AK={akVars:i,akDNSPreFetchDomain:n,init:
+      function()
+      {if(!_.i){var a=BOOMR.subscribe;a("before_beacon",_.av,null,null),a("onbeacon",_.rv,null,null),_.i=!0}return this},is_complete:
+      function()
+      {return!0}}}}()}(window);
+
+      var Languages = Languages || {};
+      Languages = {
+         'current_lang': 'en',
+          'all': [{"code":"it","title":"italian","url_code":"it-en","href":"https:\/\/www.lamborghini.com\/it-en\/","active":false},
+                  {"code":"en","title":"english","url_code":"en-en","href":"https:\/\/www.lamborghini.com\/en-en\/","active":true},
+                  {"code":"zh-hans","title":"chinese","url_code":"cn-en","href":"https:\/\/www.lamborghini.com\/cn-en\/","active":false},
+                  {"code":"ja","title":"japanese","url_code":"jp-en","href":"https:\/\/www.lamborghini.com\/jp-en\/","active":false},
+                  {"code":"de","title":"german","url_code":"de-en","href":"https:\/\/www.lamborghini.com\/de-en\/","active":false},
+                  {"code":"es","title":"spanish","url_code":"es-en","href":"https:\/\/www.lamborghini.com\/es-en\/","active":false},
+                  {"code":"fr","title":"french","url_code":"fr-en","href":"https:\/\/www.lamborghini.com\/fr-en\/","active":false},
+                  {"code":"ru","title":"russian","url_code":"ru-en","href":"https:\/\/www.lamborghini.com\/ru-en\/","active":false}]
+        };
+
+
+
+
+
+        ;(function(){
+          if(typeof angular == 'undefined') return;
+          var caPrivacy = document.querySelectorAll('.privacybox > p.ca-privacy');
+          var $caPrivacy = angular.element(caPrivacy);
+          if($caPrivacy.length > 0){
+            for(var i = 0; i < $caPrivacy.length; ++i){
+              var parent = $caPrivacy[i].parentNode.parentNode;
+              // if($caPrivacy[i].parentNode.classList.contains('privacybox')){
+                console.log(parent);
+                parent.insertBefore($caPrivacy[i].cloneNode(true), $caPrivacy[i].parentNode);
+                $caPrivacy[i].remove();
+              // }
+            }
+          }
+        }());
+
+
+        (function($){$(document).ready(function () { App.triggerEvent('::appReady') });}(jQuery));
---
+++ lambo.js
@@ -0,0 +1,18 @@
+dataLayer = [{'pageCategory': 'homepage','pageSubCategory': 'PAGE - HOMEPAGE','pageLanguage': 'en','userCountry': '','memberType': 'NoLogged','page404':'false','articleTag':''}]
+
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l]
+.push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KKLP');
+
+
+
+(function(i,s,o,b,g,r,p,a,m)
+{i['aggrObjName']=r;i[r]=i[r]||function()
+{ (i[r].q=i[r].q||[]).push(arguments)},
+i[r].l=1*new Date();i['aggrApp']=p;a=s.createElement(o),
+ m=s.getElementsByTagName(o)[0];a.async=1;a.src=(g);m.parentNode.insertBefore(a,m); i[r].aggregatorServerRoot = b; })
+ 
+(window,document,'script','https://rum.lamborghini.com/c1','https://www.lamborghini.com/rum/client.js','statsCli','lamborghiniCom');
